8a66cc45672a3b47bda9e57ce30442bdfdb766c4
Sofiia Tatarinova <sofiia.tatarinova@weka.io>
2024-02-07 16:14:03 +0200

    UI: test

--- lib/context/wizardContext.tsx
+++ lib/context/wizardContext.tsx
@@ -4,18 +4,23 @@ import { createContext, useContext, useMemo, useState } from 'react'
 
 import testConfig from '../../tfconfig.json'
 
-export type JsonValue = Record<string, string | boolean | number | string[]> | null
-export type UnfilledFields = { section: string, sectionTitle: string, fields: string[] }[] | null
+export type JsonValue = Record<
+  string,
+  string | boolean | number | string[]
+> | null
+export type UnfilledFields =
+  | { section: string; sectionTitle: string; fields: string[] }[]
+  | null
 export type WizardContextType = {
-  jsonValue: JsonValue,
-  setJsonValue: (value: JsonValue) => void,
-  unfilledFields: UnfilledFields,
-  setUnfilledFields: (value: UnfilledFields) => void,
-  selectTab: string,
+  jsonValue: JsonValue
+  setJsonValue: (value: JsonValue) => void
+  unfilledFields: UnfilledFields
+  setUnfilledFields: (value: UnfilledFields) => void
+  selectTab: string
   setSelectTab: (value: string) => void
 }
 
-const WizardContext = createContext<WizardContextType | null>(null)
+const WizardContext = createContext<WizardContextType | null>('test')
 
 const WizardContextProvider = (props: PropsWithChildren) => {
   const [jsonValue, setJsonValue] = useState<JsonValue>(null)
@@ -23,28 +28,32 @@ const WizardContextProvider = (props: PropsWithChildren) => {
   const firstPart = testConfig[0]
   const [selectTab, setSelectTab] = useState<string>(firstPart?.section)
 
-  const contextValue = useMemo(() => ({
-    jsonValue,
-    setJsonValue,
-    unfilledFields,
-    setUnfilledFields,
-    selectTab,
-    setSelectTab
-  }), [jsonValue, unfilledFields, selectTab])
+  const contextValue = useMemo(
+    () => ({
+      jsonValue,
+      setJsonValue,
+      unfilledFields,
+      setUnfilledFields,
+      selectTab,
+      setSelectTab
+    }),
+    [jsonValue, unfilledFields, selectTab]
+  )
 
   return <WizardContext.Provider value={contextValue} {...props} />
 }
 
 const useWizardContext = () => {
- const context = useContext(WizardContext)
+  const context = useContext(WizardContext)
+  console.log('context', context)
 
- if (!context) {
-   throw new Error(
-     `useWizardContext must be used within the WizardContext provider`
-   )
- }
+  if (!context) {
+    throw new Error(
+      `useWizardContext must be used within the WizardContext provider`
+    )
+  }
 
- return context
+  return context
 }
 
 export { WizardContextProvider, useWizardContext }
